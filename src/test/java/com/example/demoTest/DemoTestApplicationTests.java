8f8933dec2d0c0d51d7f0da37e33eac0494abe02
licyasana <licyasana.boumaza@univ-constantine2.dz>
2025-11-19 22:02:13 +0100

    Fix structure

--- src/test/java/com/example/demoTest/DemoTestApplicationTests.java
+++ src/test/java/com/example/demoTest/DemoTestApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.demoTest;
+
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class DemoTestApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
