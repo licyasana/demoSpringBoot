861fe0d68df6d42d310595b3fe1bcde758b19891
licyasana <licyasana.boumaza@univ-constantine2.dz>
2025-11-19 22:07:39 +0100

    Add controller test example

---
+++ src/test/java/com/example/demoTest/ControllerTest.java
@@ -0,0 +1,42 @@
+package com.example.demoTest;
+
+import org.junit.jupiter.api.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepository;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class ControllerTest {
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Test
+    @Order(1)
+    void shouldSaveStudent() {
+        Student student = new Student();
+        student.setName("Charlie");
+        student.setAddress("Algeria");
+
+        studentRepository.save(student);
+
+        assertThat(studentRepository.count()).isEqualTo(1);
+    }
+
+    @Test
+    @Order(2)
+    void shouldFindAllStudents() {
+        List<Student> students = studentRepository.findAll();
+
+        assertThat(students).hasSize(1);
+        assertThat(students.get(0).getName()).isEqualTo("Charlie");
+    }
+}
+
