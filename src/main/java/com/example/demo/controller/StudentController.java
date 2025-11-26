fc7078e4fe07b993be17491ab4f9596191893a38
licyasana <licyasana.boumaza@univ-constantine2.dz>
2025-11-26 10:53:57 +0100

    Initial push - upload project

---
+++ src/main/java/com/example/demo/controller/StudentController.java
@@ -0,0 +1,36 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import com.example.demo.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@CrossOrigin(origins = "http://localhost:3000")
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping("/add")
+    public String add(@RequestBody Student student) {
+        studentService.saveStudent(student);
+        return "New student is added";
+    }
+
+    @GetMapping("/getAll")
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+    
+    @GetMapping("/getAllUniv")
+    public List<Object> getAllStudentsUniversity() {
+        return studentService.getAllStudentsUniversity();
+    }
+
+    @GetMapping("/findStudUniv")
+    public List<Object> findStudentsByUniversity(@RequestParam String univName) {
+        return studentService.findStudentsByUniversity(univName);
+    }
+}
---
+++ src/main/java/com/example/demo/service/UniversityService.java
@@ -0,0 +1,21 @@
+package com.example.demo.service;
+
+import com.example.demo.model.University;
+import com.example.demo.repository.UniversityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class UniversityService {
+    @Autowired
+    private UniversityRepository universityRepository;
+
+    public University saveUniversity(University university) {
+        return universityRepository.save(university);
+    }
+
+    public List<University> getAllUniversities() {
+        return universityRepository.findAll();
+    }
+}
